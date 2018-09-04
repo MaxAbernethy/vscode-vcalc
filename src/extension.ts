c63074c32ba058cbb777ce4d817f829335495a15
webewildered <max.abernethy@gmail.com>
2018-09-03 17:02:15 -0700

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,434 @@
+'use strict';
+import { ExtensionContext, CancellationToken, Disposable, DocumentLink, DocumentLinkProvider, TextDocument, Range, Position, Uri, QuickPickItem,
+    languages, commands, window,  } from 'vscode';
+let clipboardy = require('clipboardy');
+
+class ValueType
+{
+    constructor(dimensions:number, length:number)
+    {
+        this.dimensions = dimensions;
+        this.length = length;
+    }
+
+    valid():boolean
+    {
+        return (this.length > 0);
+    }
+
+    dimensions:number = 0;  // 0 scalar, 1 vector, 2 matrix
+    length:number = 0;
+}
+
+function getType(length:number) : ValueType
+{
+    // Scalar
+    if (length === 1)
+    {
+        return new ValueType(0, 1);
+    }
+
+    // 2-, 3-, or 4- Vector
+    if (length <= 4)
+    {
+        return new ValueType(1, length);
+    }
+
+    // NxN matrix, n >= 3
+    let sqrt = Math.sqrt(length);
+    if (sqrt === Math.floor(sqrt))
+    {
+        return new ValueType(2, sqrt);
+    }
+
+    return new ValueType(0, 0); // invalid
+}
+
+function stringify(x:number[]): string
+{
+    let type = getType(x.length);
+    switch(type.dimensions)
+    {
+        case 0: return x[0].toString();
+        case 1: return '(' + x.join(', ') + ')';
+        case 2:
+        {
+            let cols = '';
+            for (let i = 0; i < type.length; i++)
+            {
+                cols += '(' + x.slice(i * type.length, (i + 1) * type.length).join(', ') + ')';
+                if (i < type.length - 1)
+                {
+                    cols += ', ';
+                }
+            }
+            return '(' + cols + ')';
+        }
+        default: return 'error';
+    }
+}
+
+function opPairs(a:number[], b:number[], op:(a:number, b:number)=>number): number[]
+{
+    // Check type compatibility
+    let typeA = getType(a.length);
+    let typeB = getType(b.length);
+    if (typeA.length !== typeB.length && typeA.dimensions !== 0 && typeB.dimensions !== 0)
+    {
+        return [];
+    }
+
+    // Apply op
+    let result:number[] = [];
+    let length = Math.max(a.length, b.length);
+    for (let i = 0; i < length; i++)
+    {
+        result.push(op(a[i % a.length], b[i % b.length]));
+    }
+    return result;
+}
+
+function matrixMultiply(m:number[], x:number[]): number[]
+{
+    let typeM = getType(m.length);
+    let typeX = getType(x.length);
+
+    if (typeM.length !== typeX.length)
+    {
+        return [];
+    }
+
+    let result: number[] = [];
+    for (let i = 0; i < x.length; i++)
+    {
+        let offsetM = i % 3;
+        let offsetX = Math.floor(i / 3) * 3;
+        let sum = 0;
+        for (let j = 0; j < typeM.length; j++)
+        {
+            sum += m[offsetM + j * 3] * x[offsetX + j];
+        }
+        result.push(sum);
+    }
+    return result;
+}
+
+function magnitude(x:number[]):number
+{
+    let lengthSquared = 0;
+    for (let i = 0; i < x.length; i++)
+    {
+        lengthSquared += x[i] * x[i];
+    }
+    return Math.sqrt(lengthSquared);
+}
+
+function reciprocal(x:number[]): number[]
+{
+    let rcp: number[] = [];
+    x.forEach(function(x: number) { rcp.push(1.0 / x); });
+    return rcp;
+}
+
+function normalize(x:number[]): number[]
+{
+    let invLength = 1.0 / magnitude(x);
+    let n: number[] = [];
+    x.forEach(function(x: number) { n.push(x * invLength); });
+    return n;
+}
+
+function dot(a:number[], b:number[]): number[]
+{
+    let typeA = getType(a.length);
+    let typeB = getType(b.length);
+    if (typeA.length !== typeB.length && typeA.dimensions !== 0 && typeB.dimensions !== 0)
+    {
+        return [];
+    }
+
+    let sum = 0;
+    let length = Math.max(a.length, b.length);
+    for (let i = 0; i < length; i++)
+    {
+        sum += a[i % a.length] * b[i % b.length];
+    }
+    return [sum];
+}
+
+function cross(a:number[], b:number[]): number[]
+{
+    if (a.length !== 3 || b.length !== 3)
+    {
+        return [];
+    }
+
+    return [
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    ];
+}
+
+class ContentProvider implements DocumentLinkProvider
+{
+    constructor()
+    {
+        // Initialize regular expressions
+        let numberExprStr = "(-?\\d+\\.?\\d*f?)";
+        this.numberExpr = new RegExp(numberExprStr, 'g');
+        let sepExprStr = "[()[\\]{}=\\s]";
+        this.leadingSeparatorExpr = new RegExp("^" + sepExprStr + "*");
+        this.trailingSeparatorExpr = new RegExp(sepExprStr + "*$");
+        let arrayExprStr = "(^|" + sepExprStr + "*)" + numberExprStr + "(" + sepExprStr + "*," + sepExprStr + "*" + numberExprStr + ")*";
+        this.arrayExpr = new RegExp(arrayExprStr, 'g');
+    }
+
+    provideDocumentLinks(document: TextDocument, token: CancellationToken): DocumentLink[]
+    {
+        let links: DocumentLink[] = [];
+        for (let i = 0; i < document.lineCount; i++)
+        {
+            let line = document.lineAt(i).text;
+            this.arrayExpr.lastIndex = 0; // reset the expression
+            while (true)
+            {
+                // Search for an array of numbers on the line
+                let match = this.arrayExpr.exec(line);
+                if (match === null) {
+                    break;
+                }
+
+                // Check the length of the array, don't linkify if there is an outstanding operator and this array does not match
+                let typeA = getType(this.operand.length);
+                let typeB = getType(match[0].split(',').length);
+                switch (this.operator)
+                {
+                    case 'cross': if (typeB.length !== 3) { continue; } break;
+                    case 'add':
+                    case 'subtract':
+                    case 'multiply':
+                    case 'divide':
+                    case 'dot':
+                        if (typeA.length !== typeB.length && typeA.dimensions !== 0 && typeB.dimensions !== 0)
+                        {
+                            continue;
+                        }
+                        break;
+                    default: break; // no length requirement
+                }
+
+                // Trim whitespace from the match
+                let leadingMatch = match[0].match(this.leadingSeparatorExpr);
+                let leadingSeparators = (leadingMatch === null ? 0 : leadingMatch[0].length);
+                let trailingMatch = match[0].match(this.trailingSeparatorExpr);
+                let trailingSeparators = (trailingMatch === null ? 0 : trailingMatch[0].length);
+
+                // Linkify the text
+                let range = new Range(new Position(i, match.index + leadingSeparators), new Position(i, match.index + match[0].length - trailingSeparators));
+                let commandUri = 'command:vectorcalculator.setOperand?' + JSON.stringify(match[0]);
+                links.push(new DocumentLink(range, Uri.parse(commandUri)));
+            }
+        }
+        return links;
+    }
+
+    async setOperand(operandStr: string)
+    {
+        // Parse the operand
+        this.numberExpr.lastIndex = 0;
+        let operand: number[] = [];
+        while (true)
+        {
+            let match = this.numberExpr.exec(operandStr);
+            if (match === null) {
+                break;
+            }
+            operand.push(parseFloat(match[0]));
+        }
+
+        // If there was an operation in progress, complete it
+        let result: number[] = [];
+        switch(this.operator)
+        {
+            // Arithmetic
+            case 'add': result = opPairs(this.operand, operand, function(a:number, b:number):number { return a + b; }); break;
+            case 'subtract': result = opPairs(this.operand, operand, function(a:number, b:number):number { return a - b; }); break;
+            case 'divide': result = opPairs(this.operand, operand, function(a:number, b:number):number { return a / b; }); break;
+            case 'multiply':
+            {
+                let typeA = getType(this.operand.length);
+                let typeB = getType(operand.length);
+                if (typeA.dimensions === 2 && typeB.dimensions !== 0)
+                {
+                    result = matrixMultiply(this.operand, operand);
+                }
+                else if (typeB.dimensions === 2 && typeA.dimensions !== 0)
+                {
+                    // vector times matrix, just reverse the order so it works
+                    result = matrixMultiply(operand, this.operand);
+                }
+                else
+                {
+                    result = opPairs(this.operand, operand, function(a:number, b:number):number { return a * b; });
+                }
+                break;
+            }
+
+            // Linear algebra
+            case 'dot': result = dot(this.operand, operand); break;
+            case 'cross': result = cross(this.operand, operand); break;
+
+            default: result = operand;
+        }
+
+        // Check for an error, eg. mismatched operands
+        if (result.length === 0)
+        {
+            window.showInformationMessage('vcalc: error');
+            this.clear();
+            return;
+        }
+
+        // Decide what to do next
+        while (true)
+        {
+            // Build the operator list
+            let operators: QuickPickItem[] = [];
+            let operandType = getType(result.length);
+
+            if (operandType.dimensions === 1)
+            {
+                // Vector operations
+                let labels = ['x', 'y', 'z', 'w'];
+                for (let i = 0; i < operandType.length; i++)
+                {
+                    operators.push({ label: labels[i], description : result[i].toString()});
+                }
+                operators.push({ label: 'length', description: magnitude(result).toString() });
+                operators.push({ label: 'normalize', description: stringify(normalize(result)) });
+                operators.push({ label: 'dot' });
+                if (result.length === 3)
+                {
+                    operators.push({ label: 'cross' });
+                }
+            }
+            else if (operandType.dimensions === 2)
+            {
+                // Matrix operations
+            }
+
+            // Common operations
+            let resultStr = stringify(result);
+            operators.push({ label: 'add' });
+            operators.push({ label: 'subtract' });
+            operators.push({ label: 'multiply' });
+            operators.push({ label: 'divide' });
+            operators.push({ label: 'reciprocal', description: stringify(reciprocal(result)) });
+            operators.push({ label: 'copy', description: resultStr });
+            operators.push({ label: 'insert', description: resultStr });
+
+            // Show the current value
+            window.showInformationMessage('vcalc: ' + stringify(result));
+
+            // Choose an operator
+            let operator = await window.showQuickPick(operators);
+            if (operator === undefined)
+            {
+                // Clear the state
+                this.clear();
+                return;
+            }
+
+            // Handle unary operators
+            switch (operator.label)
+            {
+                // Vector component selection
+                case 'x': result = [result[0]]; continue;
+                case 'y': result = [result[1]]; continue;
+                case 'z': result = [result[2]]; continue;
+                case 'w': result = [result[3]]; continue;
+                
+                case 'length': result = [magnitude(result)]; continue;
+                case 'reciprocal': result = reciprocal(result); continue;
+                case 'normalize': result = normalize(result); continue;
+
+                // Output
+                case 'copy':
+                    clipboardy.write(stringify(result));
+                    this.clear();
+                    break;
+
+                case 'insert':
+                    // TODO
+                    this.clear();
+                    break;
+
+                default:
+                    // Binary operator, wait for another operand
+                    this.operator = operator.label;
+                    this.operand = result;
+                    break;
+            }
+
+            // Show the current value and operator
+            if (this.operand.length > 0)
+            {
+                window.showInformationMessage('vcalc: ' + stringify(this.operand) + ' ' + this.operator + ' ...');
+            }
+
+            // Re-parse links
+            /*
+            if (window.activeTextEditor)
+            {
+                commands.executeCommand<DocumentLink[]>('vscode.executeLinkProvider', window.activeTextEditor.document.uri);
+            }*/
+
+            return;
+        }
+
+        // Clear the state
+        this.clear();
+    }
+
+    clear()
+    {
+        this.operand = [];
+        this.operator = '';
+    }
+
+    // Currently selected operand / operator
+    operand: number[] = [];
+    operator: string = '';
+
+    // Regular expressions
+    numberExpr: RegExp;
+    arrayExpr: RegExp;
+    leadingSeparatorExpr: RegExp;
+    trailingSeparatorExpr: RegExp;
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: ExtensionContext)
+{
+    const provider = new ContentProvider();
+
+	// register content provider for scheme `references`
+	// register document link provider for scheme `references`
+	const providerRegistrations = Disposable.from(
+        languages.registerDocumentLinkProvider({ language:'plaintext' }, provider)
+	);
+
+    context.subscriptions.push(providerRegistrations);
+
+    let disposable = commands.registerCommand('vectorcalculator.setOperand', (operand) => {
+        provider.setOperand(operand);
+    });
+    
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
